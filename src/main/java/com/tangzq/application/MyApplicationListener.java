8f2c4269d7c7b92a8d6abdeef1b09781f93effed
tzq668766 <tzq618766@gmail.com>
2017-10-14 01:44:00 +0800

    使用git-commit-id-plugin插件在项目中显示最近一次构建信息，方便后期跟踪

--- src/main/java/com/tangzq/application/MyApplicationListener.java
+++ src/main/java/com/tangzq/application/MyApplicationListener.java
@@ -1,5 +1,7 @@
 package com.tangzq.application;
 
+import com.tangzq.gitinfo.GitCommitInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -9,8 +11,13 @@ public class MyApplicationListener implements ApplicationListener<ContextRefresh
     @Value("${spring.profiles.active}")
     private String activeProfiles;
 
+    @Autowired
+    GitCommitInfo gitCommitInfo;
+
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         System.out.println("当前环境："+activeProfiles);
+        System.out.println("当前构建信息：");
+        System.out.println(gitCommitInfo.toString());
     }
 
 
---
+++ src/main/java/com/tangzq/gitinfo/GitCommitInfo.java
@@ -0,0 +1,37 @@
+package com.tangzq.gitinfo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.codehaus.jackson.annotate.JsonWriteNullProperties;
+
+@JsonWriteNullProperties(true)
+@Getter
+@Setter
+@ToString
+public class GitCommitInfo {
+    String tags;                    // =${git.tags} // comma separated tag names
+    String branch;                  // =${git.branch}
+    String dirty;                   // =${git.dirty}
+    String remoteOriginUrl;         // =${git.remote.origin.url}
+
+    String commitId;                // =${git.commit.id.full} OR ${git.commit.id}
+    String commitIdAbbrev;          // =${git.commit.id.abbrev}
+    String describe;                // =${git.commit.id.describe}
+    String describeShort;           // =${git.commit.id.describe-short}
+    String commitUserName;          // =${git.commit.user.name}
+    String commitUserEmail;         // =${git.commit.user.email}
+    String commitMessageFull;       // =${git.commit.message.full}
+    String commitMessageShort;      // =${git.commit.message.short}
+    String commitTime;              // =${git.commit.time}
+    String closestTagName;          // =${git.closest.tag.name}
+    String closestTagCommitCount;   // =${git.closest.tag.commit.count}
+
+    String buildUserName;           // =${git.build.user.name}
+    String buildUserEmail;          // =${git.build.user.email}
+    String buildTime;               // =${git.build.time}
+    String buildHost;               // =${git.build.host}
+    String buildVersion;             // =${git.build.version}
+
+    public GitCommitInfo() {}
+}
