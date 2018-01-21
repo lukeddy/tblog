cb43dd3b40a49fb59c0338f7f55fcba2979e0717
tzq668766 <tzq618766@gmail.com>
2018-01-21 11:30:35 +0800

    系统启动时初始化管理员账号

--- src/main/java/com/tangzq/application/MyApplicationListener.java
+++ src/main/java/com/tangzq/application/MyApplicationListener.java
@@ -1,10 +1,16 @@
 package com.tangzq.application;
 
 import com.tangzq.gitinfo.GitCommitInfo;
+import com.tangzq.model.User;
+import com.tangzq.repository.UserRepository;
+import com.tangzq.utils.CommonProps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.util.DigestUtils;
+
+import java.util.Date;
 
 /**
  * @author tangzhiqiang
@@ -17,11 +23,38 @@ public class MyApplicationListener implements ApplicationListener<ContextRefresh
     @Autowired
     GitCommitInfo gitCommitInfo;
 
+    @Autowired
+    private UserRepository userRepository;
+
+
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         System.out.println("当前环境："+activeProfiles);
         System.out.println("当前构建信息：");
         System.out.println(gitCommitInfo.toString());
+        System.out.println("系统开始初始化...");
+        initSystem();
+        System.out.printf("系统初始化完毕！");
     }
+
+    /**
+     * 初始化系统
+     */
+    public void initSystem(){
+        User u=userRepository.findByUsername(CommonProps.ADMIN_NAME);
+        if(null==u){
+            u=new User();
+            u.setUsername(CommonProps.ADMIN_NAME);
+            u.setPassword(DigestUtils.md5DigestAsHex(CommonProps.ADMIN_PWD.getBytes()));
+            u.setEmail(CommonProps.ADMIN_EMAIL);
+            u.setCreateAt(new Date());
+            u.setUpdateAt(new Date());
+            userRepository.save(u);
+            System.out.println("初始化管理员账号成功！");
+        }else{
+            System.out.println("管理员账号已经存在");
+        }
+    }
+
 
 
 }
