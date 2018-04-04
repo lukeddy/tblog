2b0b0088be72da8ca112385522f3df4a729d56a3
tzq668766 <tzq618766@gmail.com>
2018-04-04 12:06:07 +0800

    添加头像生成依赖库和示例

--- src/test/java/com/tangzq/test/utils/TestUtils.java
+++ src/test/java/com/tangzq/test/utils/TestUtils.java
@@ -1,11 +1,20 @@
 package com.tangzq.test.utils;
 
+import com.talanlabs.avatargenerator.Avatar;
+import com.talanlabs.avatargenerator.GitHubAvatar;
+import com.talanlabs.avatargenerator.IdenticonAvatar;
+import com.talanlabs.avatargenerator.cat.CatAvatar;
+import com.talanlabs.avatargenerator.layers.backgrounds.RandomColorPaintBackgroundLayer;
+import com.talanlabs.avatargenerator.layers.masks.RoundRectMaskLayer;
+import com.talanlabs.avatargenerator.layers.others.ShadowLayer;
+import com.talanlabs.avatargenerator.smiley.SmileyAvatar;
 import com.tangzq.utils.DateUtils;
 import com.tangzq.utils.GravatarUtils;
 import com.tangzq.utils.UploadUtil;
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Test;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,6 +43,27 @@ public class TestUtils {
     public void testGravatar(){
         String headerURL= GravatarUtils.makeGravatar("23424aaa@gmail.com");
         System.out.println(headerURL);
+    }
+
+    @Test
+    public void testGenerateAvatar(){
+        File file=new File("/tmp/github-"+System.currentTimeMillis()+".png");
+        Avatar avatar = GitHubAvatar.newAvatarBuilder().build();
+        avatar.createAsPngToFile(123456L,file);
+
+        File file2=new File("/tmp/smile-"+System.currentTimeMillis()+".png");
+        Avatar avatar2 = SmileyAvatar.newEyeMouthAvatarBuilder().build();
+        avatar2.createAsPngToFile(12345L,file2);
+
+        File file3=new File("/tmp/iden-"+System.currentTimeMillis()+".png");
+        Avatar avatar3 = IdenticonAvatar.newAvatarBuilder().build();
+        avatar3.createAsPngToFile(123456L,file3);
+
+        File file4=new File("/tmp/cat-"+System.currentTimeMillis()+".png");
+        Avatar avatar4 = CatAvatar.newAvatarBuilder()
+                .layers(new ShadowLayer(), new RandomColorPaintBackgroundLayer(), new RoundRectMaskLayer())
+                .padding(8).margin(8).build();
+        avatar4.createAsPngToFile(123456L,file4);
     }
 
 }
