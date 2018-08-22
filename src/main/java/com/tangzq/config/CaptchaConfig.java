ac2627e77b94e49d5451917647b8d9de22c835f0
tzq668766 <tzq618766@gmail.com>
2018-08-22 23:09:56 +0800

    尝试着解决验证码乱码问题

--- src/main/java/com/tangzq/config/CaptchaConfig.java
+++ src/main/java/com/tangzq/config/CaptchaConfig.java
@@ -28,7 +28,8 @@ public class CaptchaConfig
         properties.setProperty("kaptcha.session.key", "kaptchaCode");
         properties.setProperty("kaptcha.textproducer.char.spac", "35");
         properties.setProperty("kaptcha.textproducer.char.length", "5");
-        properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
+        //系统本身如果没有这些字体，会出现乱码
+        //properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
         properties.setProperty("kaptcha.noise.color", "white");
         Config config = new Config(properties);
         defaultKaptcha.setConfig(config);
@@ -50,7 +51,7 @@ public class CaptchaConfig
         properties.setProperty("kaptcha.textproducer.impl", "com.tangzq.common.KaptchaTextCreator");
         properties.setProperty("kaptcha.textproducer.char.spac", "5");
         properties.setProperty("kaptcha.textproducer.char.length", "6");
-        properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
+        //properties.setProperty("kaptcha.textproducer.font.names", "Arial,Courier");
         properties.setProperty("kaptcha.noise.color", "white");
         properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
         properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.ShadowGimpy");
