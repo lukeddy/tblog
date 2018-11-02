79768f8b92ac7910acca0a7ef9d7b10fdf374ba1
tzq668766 <tzq618766@gmail.com>
2018-11-02 15:02:56 +0800

    完成首页内容接口

---
+++ src/main/java/com/tangzq/controller/api/ApiHomeController.java
@@ -0,0 +1,40 @@
+package com.tangzq.controller.api;
+
+import com.tangzq.response.Result;
+import com.tangzq.service.CategoryService;
+import com.tangzq.service.TopicService;
+import com.tangzq.vo.IndexVo;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 首页API
+ * @author tangzhiqiang
+ */
+@RestController
+@RequestMapping("/api/home")
+public class ApiHomeController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @ApiOperation(value="网站首页", notes="首页内容，包括分类，文章等")
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    public Result home(@RequestBody IndexVo vo) {
+        HashMap<String,Object> map=new HashMap<>(3);
+        map.put("pager",topicService.findByPage(vo));
+        map.put("catList",categoryService.findAll());
+        map.put("indexVo",vo);
+        return Result.ok(map);
+    }
+}
